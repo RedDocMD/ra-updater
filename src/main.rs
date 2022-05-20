b9fb176f1b0dfe40a28687455a38014ec2ac1fd3
Deep Majumder <deep.majumder2019@gmail.com>
2022-05-20 09:58:54 +0530

    Added code to download ra

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::process;
+use std::path::PathBuf;
+use std::{env, process};
 
 use octocrab::models::repos::Release;
 use octocrab::Octocrab;
 use thiserror::Error;
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
 
 #[tokio::main]
 async fn main() {
@@ -13,15 +16,28 @@ async fn main() {
             process::exit(1);
         }
     };
-    for asset in release.assets {
-        println!("{}", asset.name);
-    }
+    let asset_path = match download_asset(&release).await {
+        Ok(v) => v,
+        Err(e) => {
+            eprintln!("Failed to download asset: {}", e);
+            process::exit(1);
+        }
+    };
 }
 
 #[derive(Debug, Error)]
 enum RaUpdaterError {
-    #[error("Octocrab error: {0}")]
+    #[error("octocrab error: {0}")]
     Octocrab(#[from] octocrab::Error),
+
+    #[error("reqwest error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+
+    #[error("io error: {0}")]
+    Io(#[from] std::io::Error),
+
+    #[error("Asset {0} not found")]
+    AssetNotFound(String),
 }
 
 const RA_OWNER: &str = "rust-lang";
@@ -32,4 +48,21 @@ async fn latest_release() -> Result<Release, RaUpdaterError> {
     let repo_handler = github.repos(RA_OWNER, RA_REPO);
     let latest_release = repo_handler.releases().get_latest().await?;
     Ok(latest_release)
+}
+
+const RA_ASSET_NAME: &str = "rust-analyzer-x86_64-unknown-linux-gnu.gz";
+
+async fn download_asset(release: &Release) -> Result<PathBuf, RaUpdaterError> {
+    for asset in &release.assets {
+        if asset.name == RA_ASSET_NAME {
+            let response = reqwest::get(asset.browser_download_url.clone()).await?;
+            let mut file_path = env::temp_dir();
+            file_path.push(&asset.name);
+            let mut file = File::create(&file_path).await?;
+            let response_bytes = response.bytes().await?;
+            file.write_all(&response_bytes).await?;
+            return Ok(file_path);
+        }
+    }
+    Err(RaUpdaterError::AssetNotFound(RA_ASSET_NAME.to_string()))
 }
